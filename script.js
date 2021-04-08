3f1dd00fb7b302d4e6c175796442739e955a9bb8
Dair Smith <dair.code@gmail.com>
2021-04-08 16:03:53 -0400

    first commit for lab 3 declaring variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const name = "Dair Smith";
+const age = 23;
+const birthday = "April 15";
+const detroitGC = false;
+const lifeEvents = ["I was born in Oregon.", "I went to GRCC.", "I took Tae-Kwon-Do lessons as a kid.", "I am in the Javascript bootcamp."];
+
