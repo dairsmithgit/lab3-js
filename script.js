825b96b0d4ecb8cba4697a02801cfbb00f9252eb
Dair Smith <dair.code@gmail.com>
2021-04-08 16:15:29 -0400

    if else about me section

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const name = "Dair Smith";
+const nameFirstLast = "Dair Smith";
 const age = 23;
 const birthday = "April 15";
 const detroitGC = false;
 const lifeEvents = ["I was born in Oregon.", "I went to GRCC.", "I took Tae-Kwon-Do lessons as a kid.", "I am in the Javascript bootcamp."];
+
+if (detroitGC = true) {
+    console.log("My name is " + nameFirstLast + " and I am a student at Grand Circus in Detroit, Michigan. I am currently " + age + " years old and my birthday is on " + birthday + ".");
+} else {
+    console.log("My name is " + nameFirstLast + " and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently " + age + " years old and my birthday is on " + birthday + ".");
+}
 
