6a108248862eb92ad17a46fb682371189f88c22b
Dair Smith <dair.code@gmail.com>
2021-04-11 18:12:39 -0400

    trying bonus problem

--- script1.js
+++ script1.js
@@ -2,27 +2,24 @@ let hours = prompt("Hours: ");
 let wage = prompt("Wage: ");
 
 if (hours > 40) {
-    let pay = (40 * wage) + ((hours - 40) * (wage * 1.5));
-    console.log(pay);
+  let pay = 40 * wage + (hours - 40) * (wage * 1.5);
+  console.log(pay);
 } else {
-    let pay = (hours * wage);
-    console.log(pay);
+  let pay = hours * wage;
+  console.log(pay);
 }
 
-/*
-
-DID NOT WORK - function seems exponential because it is adding
-1 * pay + 2 * pay etc until 1 million
-
+/* cannot figure out how to carry pay from function above in this part so i declared
+the example value I used to test for hours and wage above to show the equation works */
 let millionaire = 0;
 let weekCount = 0;
+let pay = 550;
 
 while (millionaire <= 1000000) {
-    millionaire += (pay * weekCount);
+    millionaire += pay;
     weekCount++;
     if (millionaire >= 1000000) {
         console.log("It took " + weekCount + " weeks to become a millionaire.");
         break;
     }
 }
-*/
